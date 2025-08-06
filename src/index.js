2b9cfaaa5c3abe2c0f0e643527f4f016ccd6606b
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-08-07 00:20:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from "./App";
 import { UserProvider } from "./context/UserContext";
 import { MarketProvider } from "./context/MarketContext";
 import { PortfolioProvider } from "./context/PortfolioContext";
+import { OrderProvider } from "./context/OrderContext";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <UserProvider>
     <MarketProvider>
       <PortfolioProvider>
-        <App />
+        <OrderProvider>
+          <App />
+        </OrderProvider>
       </PortfolioProvider>
     </MarketProvider>
   </UserProvider>
