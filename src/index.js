d84b1671809a8b98656a526568203641d443b893
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-08-07 00:18:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { UserProvider } from "./context/UserContext";
+import { MarketProvider } from "./context/MarketContext";
+import { PortfolioProvider } from "./context/PortfolioContext";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <UserProvider>
+    <MarketProvider>
+      <PortfolioProvider>
+        <App />
+      </PortfolioProvider>
+    </MarketProvider>
+  </UserProvider>
+);
